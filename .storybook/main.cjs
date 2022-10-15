b5180ac9461415e5eee31d26c92518b77452dbd5
Gabriel Borges <gbdesigns13@gmail.com>
2022-10-15 02:07:59 -0300

    Configuração Storybook

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -12,4 +12,11 @@ module.exports = {
   features: {
     storyStoreV7: true,
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === "PRODUCTION") {
+      config.base = "/lab-design-system/";
+    }
+
+    return config;
+  },
 };
